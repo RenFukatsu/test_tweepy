13026dc8b0705c418bbe6ddb13e790f57b78774b
RenFukatsu <fukatsuren88@gmal.com>
2019-04-14 22:42:12 +0900

    retweet count ok

--- retweet_count.py
+++ retweet_count.py
@@ -2,7 +2,7 @@
 
 Test tweepy by python3.6.7
 
-Description: Show 20 tweets (user, text, retweet_count) of follow people.
+Description: counts retweets.
 
 '''
 
@@ -16,7 +16,6 @@ CS = '' # API secret key / consumer_secret
 AT = '' # Access token
 AS = '' # Access token secret
 
-
 # ----------------------------------------------------------------
 # This part is to conceal my key. So you do not write this part.
 import PathModule
@@ -26,13 +25,28 @@ AT = PathModule.access_token
 AS = PathModule.access_token_secret
 # ----------------------------------------------------------------
 
-
 auth = tweepy.OAuthHandler(CK, CS) # App registration
 auth.set_access_token(AT, AS) # Account registration
 
 api = tweepy.API(auth_handler=auth) # creat a class of tweepy
 
-public_tweets = api.home_timeline()
 
-for tweet in public_tweets:
-    print("user : ", tweet.user.name, "\ntext : ", tweet.text, "\nretweet : ", tweet.retweet_count) 
+if __name__ == "__main__":
+    public_tweets = api.home_timeline()
+
+    for tweet in public_tweets:
+        try:
+            print("\nThis is retweets.\nuser : ", 
+                  tweet.retweeted_status.user.name, 
+                  "\ntext : \n", 
+                  tweet.retweeted_status.text, 
+                  "\nretweet : ", 
+                  tweet.retweet_count) 
+        except:
+            print("\nuser : ", 
+                  tweet.user.name, 
+                  "\ntext : \n", 
+                  tweet.text, 
+                  "\nretweet : ", 
+                  tweet.retweet_count) 
+
