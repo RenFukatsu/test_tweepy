1697f8c9e146f88861d0faa65869027a7e2574d0
RenFukatsu <fukatsuren88@gmail.com>
2019-05-21 18:47:39 +0900

    get json from RankingSystem.py

---
+++ client.php
@@ -0,0 +1,29 @@
+<?php
+
+$strJson = '{"seq" : 0, "command" : "get_data", "data" : { "number" : "15", "name" : "Jon"}}';
+
+$address = '127.0.0.1';
+$port = 5008;
+
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, $address, $port);
+socket_write($sock, $strJson, strlen($strJson));
+$data = "";
+do{
+    $buf = socket_read($sock, 2048);
+    //echo $buf . "\n";
+    if($buf == false){
+        break;
+    }
+    $data .= $buf;
+}while(true);
+$json = json_decode($data);
+// $J = 0;
+// echo $json->{$J}->{"id"} , "\n";
+for($i = 0; $i <100; $i++){
+    echo $json->{$i}->{"id"} . "\t" . $json->{$i}->{"retweet_count_change"} . "\n";
+}
+
+socket_close($sock);
+?>
